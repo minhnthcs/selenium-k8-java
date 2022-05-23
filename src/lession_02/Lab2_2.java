873ee23d74b1bc14a7d43d6a6232720a8c779630
mason <mason@arevolabs.com>
2022-05-23 10:49:43 +0700

    add Lab2_2: check odd/even number

--- src/lession_02/Lab2_2.java
+++ src/lession_02/Lab2_2.java
@@ -7,7 +7,7 @@ public class Lab2_2 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please input a Integer number: ");
         int userInput = scanner.nextInt();
-        String msg = (userInput % 2 == 0)? "User inputted an even number" : "User inputted an odd number";
+        String msg = (userInput %2 == 0)? "User inputted an even number" : "User inputted an odd number";
         System.out.println(msg);
     }
 }
