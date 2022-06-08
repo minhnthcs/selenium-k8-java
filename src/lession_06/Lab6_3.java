50f260c6125b23012c65367f71cd75b22449bee2
mason <mason@arevolabs.com>
2022-06-08 10:53:34 +0700

    add labs of Lession6

---
+++ src/lession_06/Lab6_3.java
@@ -0,0 +1,9 @@
+package lession_06;
+
+public class Lab6_3 {
+
+    public static void main(String[] args) {
+        StringUtils lab6_3 = new StringUtils("12345nabcdada8914014-ahafaf-anafa678");
+        lab6_3.digitsExtract();
+    }
+}
---
+++ src/lession_06/Lab6_4.java
@@ -0,0 +1,9 @@
+package lession_06;
+
+public class Lab6_4 {
+
+    public static void main(String[] args) {
+        StringUtils lab6_4 = new StringUtils("httpf://google.net");
+        lab6_4.checkUrl();
+    }
+}
